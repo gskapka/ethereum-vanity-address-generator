63dee6c931d14b475c38aaf9bd28b79ba35b5f3e
Greg Kapka <greg@oraclize.it>
2020-08-05 11:35:52 +0100

    ref(app): <- rm that dir entirely & other unused `/src` mods

--- src/utils.rs
+++
@@ -1,9 +0,0 @@
-use std::fmt::Debug;
-
-// Simple monad logger. Logs contents & returns monad unmolested.
-pub fn log_monad_contents<T>(m: T) -> T 
-    where T: Debug
-{
-    println!("{:?}", m);
-    m
-}
