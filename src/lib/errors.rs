d969621399c0e22cacfdd83e5dd089b0e284e833
Greg Kapka <greg@oraclize.it>
2020-08-05 15:52:57 +0100

    feat(errors): <- adds recv error type to that enum

--- src/lib/errors.rs
+++ src/lib/errors.rs
@@ -4,13 +4,15 @@ use std::fmt;
 pub enum AppError {
     Custom(String),
     CryptoError(secp256k1::Error),
+    RecvError(std::sync::mpsc::RecvError),
 }
 
 impl fmt::Display for AppError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         let msg = match *self {
             AppError::Custom(ref msg) => msg.to_string(),
-            AppError::CryptoError(ref e) => format!("✘ Crypto Error!\n✘ {}", e),
+            AppError::RecvError(ref e) => format!("✘ Recv error: {}", e),
+            AppError::CryptoError(ref e) => format!("✘ Crypto Error: {}", e),
 
         };
         f.write_fmt(format_args!("{}", msg))
@@ -20,5 +22,11 @@ impl fmt::Display for AppError {
 impl From<secp256k1::Error> for AppError {
     fn from(e: secp256k1::Error) -> AppError {
         AppError::CryptoError(e)
+    }
+}
+
+impl From<std::sync::mpsc::RecvError> for AppError {
+    fn from(e: std::sync::mpsc::RecvError) -> AppError {
+        AppError::RecvError(e)
     }
 }
